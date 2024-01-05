d69131182eea7ba49c7a580b66603087d36cd32d
Rai Pritesh <84077582+mrjayrai@users.noreply.github.com>
2024-01-05 11:39:07 +0530

    Create Find_Consecutive_Integers_from_a_Data_Stream.java

---
+++ Goldman Sachs/Find_Consecutive_Integers_from_a_Data_Stream.java
@@ -0,0 +1,21 @@
+class DataStream {
+    private int val, k, count;
+    public DataStream(int value, int k) {
+        this.val = value;
+        this.k = k;
+        this.count = 0;
+    }
+
+    public boolean consec(int num) {
+        count = (num == val)? count + 1 : 0;
+        return count >= k;
+        
+    }
+}
+
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * DataStream obj = new DataStream(value, k);
+ * boolean param_1 = obj.consec(num);
+ */
