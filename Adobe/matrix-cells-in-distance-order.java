4f3c133b7b4fb28d07eb9ed54fcbfb87289b7705
Rai Pritesh <84077582+mrjayrai@users.noreply.github.com>
2024-01-28 13:34:08 +0530

    Create matrix-cells-in-distance-order.java

---
+++ Adobe/matrix-cells-in-distance-order.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int[][] result = new int[R * C][];
+        result[0] = new int[] {r0, c0};            
+        int resultIdx = 1;
+        int lim = Math.max( r0, R-r0-1 ) + Math.max( c0, C-c0-1 );
+		                                           
+        for (int dist = 1; dist <= lim; dist++) { 
+            int r = r0 - dist;
+            int c = c0;
+            
+            for (int count = dist; count > 0; count--) {    
+                if (r >= 0 && c >= 0)  result[resultIdx++] = new int[] {r, c};
+                r++;
+                c--;
+            }
+            
+          
+            for (int count = dist; count > 0; count--) {
+                if (r < R && c >= 0)  result[resultIdx++] = new int[] {r, c};
+                r++;
+                c++;
+            }
+            
+            
+            for (int count = dist; count > 0; count--) {
+                if (r < R && c < C)  result[resultIdx++] = new int[] {r, c};
+                r--;
+                c++;
+            }
+            
+            
+            for (int count = dist; count > 0; count--) {
+                if (r >= 0 && c < C)  result[resultIdx++] = new int[] {r, c};
+                r--;
+                c--;
+            }
+        }
+        return result;
+    }
+}
